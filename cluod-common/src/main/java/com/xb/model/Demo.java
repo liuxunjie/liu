b2346c968795f51ba9b9759e2ea6edc7f82628ad
liuxunjie <1146595231@qq.com>
2019-05-15 22:26:58 +0800

    22

--- cluod-common/src/main/java/com/xb/model/Demo.java
+++ cluod-common/src/main/java/com/xb/model/Demo.java
@@ -1,4 +1,13 @@
 package com.xb.model;
 
 public class Demo {
+    private Integer userId;
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
 }
